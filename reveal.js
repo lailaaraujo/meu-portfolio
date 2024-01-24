9f65406ac8ddf78794c6972220bb5fa06c09c9bc
Laila Araujo <100047373+lailaaraujo@users.noreply.github.com>
2024-01-24 09:54:22 -0300

    subi arquivos do meu portfolio

---
+++ reveal.js
@@ -0,0 +1,15 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE // 
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000, 
+    distance:'90px'
+})
+
+revelar.reveal('.efeito-img-topo',
+{
+    duration: 2000, 
+    distance:'90px' ,
+    delay: 500
+})
